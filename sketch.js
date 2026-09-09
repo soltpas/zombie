d1d91bce12a1f74e9920315c2db75ed5c774b659
soltpas <sshuku252192@shukutoku.ed.jp>
2026-09-09 08:53:40 +0000

    a

--- sketch.js
+++ sketch.js
@@ -8,18 +8,19 @@ let zx = [0];
 let zy = [0];
 let zz = [0];
 let zl = [false];
+let r;
+let rr = false;
 
 function setup() {
     createCanvas(1500, 800);
-    imageMode(CENTER);
 }
 
 function draw() {
-    background(10,100,50,100)
+    background(10,100,50,50)
     for (let i = 0;i < bx.length;i++){
         if (bl[i] == true){
             bz[i] += 1;
-            bs[i] = 50 - bz[i] / 2;
+            bs[i] = 100 - bz[i];
             if (bz[i] >= 50){
                 by[i] += 0.5;
             }
@@ -32,15 +33,31 @@ function draw() {
             }
         }
     }
+    if (rr == true){
+        if(millis() % 1000 <= 50){
+            textSize(100);
+            text("リロード中",500,400)
+        }
+        if(millis() - r >= 3000){
+            bc = 0;
+            rr = false;
+        }
+    }
 }
 
 function keyPressed() {
-    if (key == "d"){
-        bx[bc] = mouseX;
-        by[bc] = mouseY;
-        bz[bc] = 0;
-        bs[bc] = 50;
-        bl[bc] = true;
-        bc += 1;
+    if (key == "d" && rr == false){
+        if (bc < 30){
+            bx[bc] = mouseX;
+            by[bc] = mouseY;
+            bz[bc] = 0;
+            bs[bc] = 100;
+            bl[bc] = true;
+            bc += 1;
+        }
+    }
+    if (key == "f"){
+        r = millis();
+        rr = true;
     }
 }
