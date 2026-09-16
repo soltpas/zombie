564903b073fa84f0de62b1cedc735a654338edc6
soltpas <sshuku252192@shukutoku.ed.jp>
2026-09-16 09:21:34 +0000

    a

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ let ux = [0];
 let uy = [0];
 let uz = [0];
 let ul = [false];
-let ut = [0];
 let ui;
+let us = [0];
 let st = 0;
 let r;
 let rr = false;
@@ -24,14 +24,22 @@ function setup() {
 }
 
 function draw() {
-    background(10,100,50,50)
-    textSize(50);
-    fill("red");
-    text("弾数:" + (30 - bc),10,50);
-    for (let i = 0;i < bx.length;i++){
+    background(10,30,100,50)
+    for (let i = 0;i < ul.length;i++){
+        if (ul[i] == true){
+            uz[i] -= 1;
+            uy[i] += 0.5;
+            us[i] = 500 - uz[i];
+            image(ui,ux[i]-a,uy[i],us[i],us[i]);
+            if (uz[i] <= 0){
+                ul[i] = false;
+            }
+        }
+    }
+    for (let i = 0;i < bl.length;i++){
         if (bl[i] == true){
             bz[i] += 1;
-            bs[i] = 100 - bz[i];
+            bs[i] = (500 - bz[i]*10) // 5;
             if (bz[i] >= 50){
                 by[i] += 0.5;
             }
@@ -44,34 +52,41 @@ function draw() {
             }
         }
     }
-    for (let i = 0;i < ux.length;i++){
+    for (let i = 0;i < ul.length;i++){
         if (ul[i] == true){
-            if(millis() - ut[i] >= 1000){
-                uz[i] -= 1;
-                uy[i] += 1;
-                ut[i] = millis();
-                Image(ui,ux[i],uy[i],50-uz[i],50-uz[i]);
+            for (let j = 0;j < bl.length;j++){
+                if (bl[j] == true &&
+                    bx[j] - a - ux[i] <= us[i] + 10 &&
+                    bx[j] - a - ux[i] >= -us[i] - 10 &&
+                    by[j] - uy[i] <= us[i] + 10 &&
+                    by[j] - uy[i] >= -us[i] - 10 && bz[j] - uz[i] <= 10 && bz[j] + uz[i] >= 10){
+                    ul[i] = false;
+                    bl[j] = false;
+                    break;
+                }
             }
         }
     }
-    if (millis() - st > 1500){
+    if (millis() - st > 2000){
         st = millis() - int(random(100,1000,100));
         ux.push(random(-750,2250));
         uy.push(0);
         ul.push(true);
-        ut.push(millis());
-        uz.push(0);
+        uz.push(500);
     }
     if (rr == true){
         if(millis() % 1000 <= 50){
             textSize(100);
-            text("リロード中",500,400)
+            text("RELOADING...",500,400)
         }
         if(millis() - r >= 3000){
             bc = 0;
             rr = false;
         }
     }
+    textSize(50);
+    fill("red");
+    text("弾数:" + (30 - bc),10,50);
 }
 
 function keyPressed() {
